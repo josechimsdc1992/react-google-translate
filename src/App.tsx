4e2e35d3de0ca16e2689738714fd5f43c84b96f5
Jose Chim <52678389+josechim@users.noreply.github.com>
2024-07-01 22:27:45 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Google Translate</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
